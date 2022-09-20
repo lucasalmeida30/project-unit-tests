bee29f61daf879d53a1012750d5f95ea4e4776f7
lucasalmeida30 <lucas_almeida12@icloud.com>
2022-09-20 14:43:22 -0300

    iniciando o proejto

--- src/average.js
+++ src/average.js
@@ -10,7 +10,9 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
+javascript
+const average = () => {
 
-const average = () => {};
+};
 
 module.exports = average;
